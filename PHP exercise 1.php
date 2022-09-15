d48c3adc172ffec43810b5603820200f4ce69f16
Gemma-lyn Cuilan <92642445+GemmalynCuilan@users.noreply.github.com>
2022-09-15 11:11:55 +0800

    Add files via upload

---
+++ PHP exercise 1.php
@@ -0,0 +1,19 @@
+<?php
+$x = 10;
+$y = 7;
+
+$result = $x+$y;
+echo "$x + $y = $result \n";
+
+$result = $x-$y;
+echo "$x - $y = $result\n";
+
+$result = $x*$y;
+echo "$x * $y = $result\n";
+
+$result = $x/$y;
+echo "$x / $y = $result\n";
+
+$result = $x%$y;
+echo "$x % $y = $result\n";
+	?>
